aa3acc29cae9b7e7568397c7bd81ef942983c3aa
saluumaa <salumahassan02@gmail.com>
2023-09-02 08:56:06 +0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,13 @@ Rails.application.routes.draw do
     end
   end
 
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: [:index] do
-  #       resources :posts, only: [:index] do
-  #         resources :comments, only: [:index, :create]
-  #       end
-  #     end
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
